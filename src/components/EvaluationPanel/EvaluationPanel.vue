7f7076d5263e785a3ff9f09ab244a5d380637639
re4388 <re4388@gmail.com>
2019-07-01 15:49:34 +0800

    Add styling in EvaluationPanel.vue

--- src/components/EvaluationPanel/EvaluationPanel.vue
+++ src/components/EvaluationPanel/EvaluationPanel.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
-    <div>
+  <div id="model-overview-panel" class="d-flex-col text-light">
+    <div class="border m-3">
       <h2>My first image Dataset_Model 1</h2>
     </div>
     <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae at nemo iusto nostrum, tenetur fugit commodi, quod soluta reprehenderit et sunt voluptatem sint deserunt consequuntur, rem error saepe voluptatibus expedita.
+      <p id="page-content" class="px-3 text-light text-left">
+        The Model evaluation report is created at 09:21, 2019/05/17. In this analysis, We analyzed 4463 image, and these
+        images were divided in 5 fold for training dataset and validation dataset. It is take 10hr for training . The
+        prediction performance of model are shown in following table.
       </p>
+
+
     </div>
   </div>
 
@@ -17,22 +22,18 @@
   import GraphDisplay from './GraphDisplay.vue'
 
   export default {
-    name: 'EvaluationPanel',
-    components: {
-
-    },
-    methods: {
-
-    },
-    data() {
-      return {
-
-      }
-    }
+    name: 'EvaluationPanel'
   }
 
 </script>
 
 <style lang="scss" scoped>
+#model-overview-panel {
+min-height: 100%;
+background-color: gray;
+}
+#page-content {
+color: black;
+}
 
 </style>
