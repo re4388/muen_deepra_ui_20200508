34f4ca700ff274ff41a7782ee94d41e79df7b22c
re4388 <re4388@gmail.com>
2019-07-02 09:30:10 +0800

    Add Flexbox in EvaluationPanel

--- src/components/EvaluationPanel/EvaluationPanel.vue
+++ src/components/EvaluationPanel/EvaluationPanel.vue
@@ -1,18 +1,25 @@
 <template>
-  <div id="model-overview-panel" class="d-flex-col text-light">
-    <div class="border m-3">
+  <div class="container text-light bg-color">
+
+    <div class="border mt-3 item">
       <h2>My first image Dataset_Model 1</h2>
     </div>
-    <div>
-      <p id="page-content" class="px-3 text-light text-left">
+
+    <div class="item">
+      <p id="page-content" class="px-3  mb-1 text-light text-left">
         The Model evaluation report is created at 09:21, 2019/05/17. In this analysis, We analyzed 4463 image, and these
         images were divided in 5 fold for training dataset and validation dataset. It is take 10hr for training . The
         prediction performance of model are shown in following table.
       </p>
-      <metrics-display />
-      <graph-display />
-
     </div>
+    <div class="item">
+      <metrics-display />
+    </div>
+
+    <div class="item">
+      <graph-display />
+    </div>
+
   </div>
 
 
@@ -24,7 +31,7 @@
 
   export default {
     name: 'EvaluationPanel',
-    components:{
+    components: {
       MetricsDisplay,
       GraphDisplay
     }
@@ -33,12 +40,34 @@
 </script>
 
 <style lang="scss" scoped>
-#model-overview-panel {
-min-height: 100%;
-background-color: gray;
-}
-#page-content {
-color: black;
-}
+  .bg-color {
+    // min-height: 100%;  
+    background-color: gray;
+  }
+
+  #page-content {
+    color: black;
+  }
+
+  .container {
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    overflow-y: scroll;
+  }
+
+  .item {
+    // background: tomato;
+    // padding: 5px;
+    // width: 50px;
+    // height: 150px;
+    // margin-top: 10px;
+    // line-height: 150px;
+    // color: white;
+    // font-weight: bold;
+    font-size: 1 em;
+    // text-align: center;
+  }
 
 </style>
