ec18a431f46581ed56cc7e5491a5e0c8ec1c413f
re4388 <re4388@gmail.com>
2019-07-17 12:22:05 +0800

    UPDATE: layout update

--- src/components/EvaluationPanel/InfoDisplay/ThresholdAdjustment.vue
+++ src/components/EvaluationPanel/InfoDisplay/ThresholdAdjustment.vue
@@ -1,72 +1,80 @@
 <template>
-  <div class="pr-5">
-
-    <div class="mt-5">
-      <form class="range-field my-1 w-40">
-        <label for="">Threshold Value</label> <br>
-        <input type="range" min="0" max="1" step="0.1" v-model.lazy.number="slider"
-          @change="ThresholdChange" />
+  <div>
+    <div>
+      <form class="range-field">
+        <label for>Threshold Value</label>
+        <br />
+        <input
+          type="range"
+          min="0"
+          max="1"
+          step="0.1"
+          v-model.lazy.number="slider"
+          @change="ThresholdChange"
+        />
         <h2 class="slider">{{ slider }}</h2>
       </form>
     </div>
 
-    <div class="pt-2">
+    <div>
       <h3>
-        Image: <span> {{ imageNumber}} </span><br>
-        Presison: {{precision}} <br>
-        Recall: {{recall}} <br></h3>
+        Image:
+        <span>{{ imageNumber}}</span>
+        <br />
+        Presison: {{precision}}
+        <br />
+        Recall: {{recall}}
+        <br />
+      </h3>
     </div>
-
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'ThresholdAdjustment',
-    props: {
-      thresholdData: {
-        type: Object,
-      }
-    },
+export default {
+  name: "ThresholdAdjustment",
+  props: {
+    thresholdData: {
+      type: Object
+    }
+  },
 
-    data() {
-      return {
-        slider: 0,
-        imageNumber: 0,
-        precision: 0,
-        recall: 0
-      }
-    },
+  data() {
+    return {
+      slider: 0,
+      imageNumber: 0,
+      precision: 0,
+      recall: 0
+    };
+  },
 
-    created() {
-      this.updateData()
+  created() {
+    this.updateData();
+  },
+  methods: {
+    ThresholdChange() {
+      let val = this.slider;
+      this.$emit("threshold-change", {
+        result: val
+      });
     },
-    methods: {
-      ThresholdChange() {
-        let val = this.slider
-        this.$emit('threshold-change', {
-          result: val
-        })
-      },
-      updateData(){
-        this.imageNumber = this.thresholdData.image
-        this.precision = this.thresholdData.precision
-        this.recall = this.thresholdData.recall
-      }
-    },
-    watch: {
-      slider() {
-        this.precision = this.precision - this.slider * 20
-        this.recall = this.recall + this.slider * 5
-      }
+    updateData() {
+      this.imageNumber = this.thresholdData.image;
+      this.precision = this.thresholdData.precision;
+      this.recall = this.thresholdData.recall;
+    }
+  },
+  watch: {
+    slider() {
+      this.precision = this.precision - this.slider * 20;
+      this.recall = this.recall + this.slider * 5;
     }
   }
-
+};
 </script>
 
 <style scoped>
-  ul li {
-    list-style-type: none;
-  }
-
+ul li {
+  list-style-type: none;
+}
 </style>
--- src/components/EvaluationPanel/TabsInfo/Tabs.vue
+++ src/components/EvaluationPanel/TabsInfo/Tabs.vue
@@ -1,50 +1,53 @@
 <template>
-  <div class="d-flex flex-column">
-    <!-- tabs -->
-    <ul class="d-flex flex-row breadcrumb">
-      <li 
-      v-for="tab in tabs" 
-      :key="tab.id">
-        <a 
-        href="#" 
-        @click.prevent="changeView(tab)">
-          {{ tab.name }}
-        </a>
-      </li>
-    </ul>
+  <!-- tabs 麵包屑 -->
+  <div class="container">
+    <div class="row align-self-center">
+      <div class="col-12 pl-1 pt-2">
+        <ul class=" pt-0 breadcrumb text-black-50 mb-1 pb-1">
+          <li v-for="tab in tabs" :key="tab.id">
+            <a href="#" @click.prevent="changeView(tab)">{{ tab.name }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
 
-    <!-- individual tab component -->
-    <Tab 
-    v-for="tab in tabs" 
-    :key="tab.id" 
-    :name="tab.name" 
-    :current-view="currentView" 
-    class="currentView">
+    <!-- 每一個lable/tab 的元件們 -->
+    <Tab
+      v-for="tab in tabs"
+      :key="tab.id"
+      :name="tab.name"
+      :current-view="currentView"
+      class="currentView container"
+    >
+      <!-- tab / label的 標題 -->
+      <div class="row">
+        <h3 class="col-12 text-center mt-0 p-2">{{ tab.name }}</h3>
+      </div>
 
-      <!-- render individual tab name -->
-      <h3 class="text-left ml-2">{{ tab.name }}</h3>
+      <!-- MetricsDisplay -->
+      <div class="row">
+        <MetricsDisplay :metrics-data=" tab.metrics" class="col-12"></MetricsDisplay>
+      </div>
 
-      <!-- render MetricsDisplay component -->
-      <MetricsDisplay 
-      :metrics-data=" tab.metrics"
-      >
-      </MetricsDisplay>
-
-      <!-- render GraphDisplay component -->
-      <div class="mb-5 row">
-        <div class="col-md-6">
-          <GraphDisplay 
-          :graph-data="tab.grpah" 
-          :new-threshold="newThreshold">
-          </GraphDisplay>
+      <!-- GraphDisplay -->
+      <div class="row">
+        <div class="col-6">
+          <GraphDisplay :graph-data="tab.grpah" :new-threshold="newThreshold"></GraphDisplay>
         </div>
 
-        <!-- ThresholdAdjustment component -->
-        <div class="col-md-6">
-          <ThresholdAdjustment 
-          :threshold-data="tab.threshold" 
-          @threshold-change="ThresholdChange">
-          </ThresholdAdjustment>
+        <!-- ThresholdAdjustment -->
+        <div class="col-6">
+          <div class="flex-column">
+            <div class="col-10"></div>
+
+            <ThresholdAdjustment
+              :threshold-data="tab.threshold"
+              @threshold-change="ThresholdChange"
+            ></ThresholdAdjustment>
+
+            <div class="col-2 mt-5"></div>
+            <b-button variant="outline-dark">Confusion Matrix</b-button>
+          </div>
         </div>
       </div>
     </Tab>
@@ -52,82 +55,80 @@
 </template>
 
 <script>
-  // import the fake data 
-  import tabData from '@/components/EvaluationPanel/TabsInfo/Tab-data.json'
-  // import tabData from '@/components/EvaluationPanel/TabsInfo/Tab-data.js'
+// import the fake data
+import tabData from "@/components/EvaluationPanel/TabsInfo/Tab-data.json";
+// import tabData from '@/components/EvaluationPanel/TabsInfo/Tab-data.js'
 
-  // import components
-  import Tab from './Tab'
-  import MetricsDisplay from '../InfoDisplay/MetricsDisplay'
-  import GraphDisplay from '../InfoDisplay/GraphDisplay'
-  import ThresholdAdjustment from '../InfoDisplay/ThresholdAdjustment'
+// import components
+import Tab from "./Tab";
+import MetricsDisplay from "../InfoDisplay/MetricsDisplay";
+import GraphDisplay from "../InfoDisplay/GraphDisplay";
+import ThresholdAdjustment from "../InfoDisplay/ThresholdAdjustment";
 
-  export default {
-    name: 'Tabs',
+export default {
+  name: "Tabs",
 
-    components: {
-      Tab,
-      MetricsDisplay,
-      GraphDisplay,
-      ThresholdAdjustment,
+  components: {
+    Tab,
+    MetricsDisplay,
+    GraphDisplay,
+    ThresholdAdjustment
+  },
+
+  data() {
+    return {
+      tabs: tabData.content,
+      views: [], // e.g. => [ 'AllTabInfo','Tab-1info','Tab-2info','Tab-3info','Tab-4info' ]
+      currentView: "",
+      newThreshold: 0
+    };
+  },
+
+  mounted() {
+    this.getView();
+    this.currentView = this.views[0];
+  },
+  methods: {
+    getView() {
+      this.views = this.tabs.map(key => {
+        return key.name;
+      });
     },
-
-    data() {
-      return {
-        tabs: tabData.content,
-        views: [], // e.g. => [ 'AllTabInfo','Tab-1info','Tab-2info','Tab-3info','Tab-4info' ]
-        currentView: '',
-        newThreshold: 0,
-      }
+    changeView(obj) {
+      this.currentView = obj.name;
     },
+    ThresholdChange(obj) {
+      this.newThreshold = obj.result;
+    }
+  }
+};
+</script>
 
-    mounted() {
-      this.getView()
-      this.currentView = this.views[0]
-    },
-    methods: {
-      getView() {
-        this.views = this.tabs.map(key => {
-          return key.name
-        })
-      },
-      changeView(obj) {
-        this.currentView = obj.name
-      },
-      ThresholdChange(obj) {
-        this.newThreshold = obj.result
+
+
+
+
+
+<style lang="scss" scoped>
+ul.breadcrumb {
+  list-style: none;
+  background-color: rgb(128, 128, 128);
+  li {
+    // display: inline;
+    font-size: 18px;
+    + li:before {
+      padding: 5px;
+      color: rgb(8, 8, 8);
+      content: " | ";
+    }
+    a {
+      color: #080808;
+      text-decoration: none;
+      &:hover {
+        color: #01447e;
+        text-decoration: underline;
       }
     }
   }
-
-</script>
-
-<style scoped>
-  ul.breadcrumb {
-    padding: 1px 10px;
-    list-style: none;
-    background-color: rgb(128, 128, 128);
-  }
-
-  ul.breadcrumb li {
-    display: inline;
-    font-size: 15px;
-  }
-
-  ul.breadcrumb li+li:before {
-    padding: 8px;
-    color: black;
-    content: "/\00a0";
-  }
-
-  ul.breadcrumb li a {
-    color: #000508;
-    text-decoration: none;
-  }
-
-  ul.breadcrumb li a:hover {
-    color: #01447e;
-    text-decoration: underline;
-  }
-
+}
 </style>
