6a369e408a9e90d3abfcf49f75f89cc41625d6a4
joanne <prince811009fy@gmail.com>
2019-07-18 17:14:30 +0800

    Update: extract `ImageBox` to a individual file

---
+++ src/components/SideBarMenuRight/ImageBox.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="image-box">
+    <div class="box">
+      <div class="title">
+        <div class="imgList" id="imgList">
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/viewer-img.png" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/viewer-img.png" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1_backup.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1_backup.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1_backup.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1_backup.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1_backup.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1_backup.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/viewer-img.png" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/viewer-img.png" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/viewer-img.png" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/viewer-img.png" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1_backup.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1_backup.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1_backup.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1_backup.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1.jpg" height="40px"></div>
+          <div class="x-rayFilms"><div class="overlayer"></div><img src="../../assets/x-ray1.jpg" height="40px"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>   
+     
+<script>
+export default {
+  name: 'ImageBox',
+  components: {
+  },
+}
+</script>
+
+<style lang="scss">
+// ImageList
+.imgList {
+  width: 300px;
+  height: 300px;
+  overflow: scroll;
+  margin: 0 auto;
+  list-style-type: none;
+  text-align: left;
+  position: relative;
+}
+
+.imgList::-webkit-scrollbar { 
+    display: none; 
+}
+
+.box {
+  width: 300px;
+  height: 300px;
+  overflow: hidden;
+  position: fixed;
+  top: 350px;
+  right: 150px;
+  padding: 0px;
+}
+
+.title {
+  background: #000;
+  width: 100%;
+  position: absolute;
+  right: -400px;
+  transition: all 1s;
+  overflow: hidden;
+}
+
+.show {
+  right: 0px;
+}
+
+.x-rayFilms {
+  box-sizing: border-box;
+  width: 60px;
+  height: 60px;
+  display: inline-flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  justify-content: center;
+  padding: 3px;
+  z-index: 9;
+  position: relative;
+}
+
+.x-rayFilms:hover::after {
+  content: "";
+  display: inline-flex;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: #42ab427d;
+}
+</style>
