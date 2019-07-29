6ada1a8a57c89c725baa3ba72f3852a769d5e8ce
nale <nale@muenai.com>
2019-07-29 16:10:41 +0800

    MAINT: push uncommitted file

---
+++ src/store/modules/Model.js
@@ -0,0 +1,39 @@
+import { initializeState } from './index'
+
+const namespaced = true;
+
+const defaultState = {
+  currentModel: {
+    info: null
+  }
+}
+
+const state = {}
+
+initializeState(state, defaultState)
+
+const getters = {
+  currentModel() {
+    return state.currentModel
+  },
+}
+
+const mutations = {
+  SET_CURRENT_MODEL(state, payload) {
+    state.currentModel = payload
+  }
+}
+
+const actions = {
+  setCurrentModel({ commit }, payload) {
+    commit('SET_CURRENT_MODEL', payload)
+  }
+}
+
+export default {
+  namespaced,
+  state,
+  getters,
+  mutations,
+  actions
+}
