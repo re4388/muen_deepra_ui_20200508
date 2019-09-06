4afe145a34928f67312e2f0c5858fe0b64613e71
re4388 <re4388@meunai.com>
2019-09-06 14:56:23 +0800

    UPDATE PipelineElement.vue unit test complete

--- src/components/EvaluationPanel/__tests__/EvaluationPanel.test.js
+++ src/components/EvaluationPanel/__tests__/EvaluationPanel.test.js
@@ -6,11 +6,8 @@ import VueRouter from 'vue-router'
 const localVue = createLocalVue()
 localVue.use(VueRouter)
 
-
-
 import EvaluationPanel from '@/components/EvaluationPanel/EvaluationPanel.vue'
 import Tabs from '@/components/EvaluationPanel/TabsInfo/Tabs.vue'
-
 
 
 describe('EvaluationPanel.vue', () => {
@@ -60,7 +57,6 @@ describe('EvaluationPanel.vue', () => {
 
 
 
-
   // Note: emit and call method, this is more like a test for implementation
   it('communicate: receive the model-data from Tabs and invoke method properly', () => {
     // need to mock a method called and mount with component
@@ -71,7 +67,7 @@ describe('EvaluationPanel.vue', () => {
         modelInfo
       }
     })
-    // find Tabs component and emit 'model-data'
+    // use "Tabs" component to emit 'model-data'
     wrapper.find(Tabs).vm.$emit('model-data')
 
     // check if the method "modelInfo" is called
@@ -112,6 +108,9 @@ describe('EvaluationPanel.vue', () => {
     // check when we have only one folder, we set the number to 1
     expect(EvaluationPanel.computed.folderNumber.call(onlyOneFdoler)).toBe(1)
   })
+
+
+
 
 })
 
